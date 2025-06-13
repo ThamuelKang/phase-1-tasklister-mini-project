1bb1d481c01aeabeccda35443e1cc8eed3bd5bc9
Samuel Kang <samuel.june.kang@gmail.com>
2025-06-13 01:15:20 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const form = document.getElementById("create-task-form")
+const taskValue = document.getElementById("new-task-description")
+const todoList = document.getElementById("tasks")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newTask = document.createElement('p')
+  newTask.textContent = taskValue.value.trim()
+  todoList.append(newTask)
+  form.reset()
+})
+
